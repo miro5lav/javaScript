6f6606aaa5e6945d267a3625593b2b8848985b28
Miro5law <mir.radziszewski@wp.eu>
2017-11-23 21:16:27 +0100

    Building distribution fo production environment.

---
+++ webpack.config.prod.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+        // Eliminate duplicate packages when generating bundle
+        new webpack.optimize.DedupePlugin(),
+
+            // Minify JS
+            new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
